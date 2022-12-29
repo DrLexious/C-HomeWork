2d69267442278fecfbe2ee023d429f0d2600b862
Alex <120469966+DrLexious@users.noreply.github.com>
2022-12-29 18:59:39 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,36 +1,76 @@
-﻿/*// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.Write("enter a three-digit number: ");
-int number = Convert.ToInt16(Console.ReadLine());
-string stringNumber = Convert.ToString(number);
-Console.Write("second digit this number is - "+stringNumber[1]);
+﻿/*// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Enter a five-digit number: ");
+string? number = Console.ReadLine();
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
+    void CheckingNumber(string number)
+    {
+        if (number[0]==number[4] || number[1]==number[3])
+        {
+            Console.WriteLine($"Number: {number} - is palindrome.");
+        }
+        else Console.WriteLine($"Number: {number} - is not a palindrome");
+    }
+}
+else Console.WriteLine("This number is not five digits");
 */
-/*// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*//Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int xA = Coordinate("x", "A");
+int yA = Coordinate("y", "A");
+int zA = Coordinate("z", "A");
+int xB = Coordinate("x", "B");
+int yB = Coordinate("y", "B");
+int zB = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Enter coordinate {coorName} of point {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+double Decision(double xA, double xB, 
+                double yA, double yB, 
+                double zA, double zB)
+{
+  return Math.Sqrt(Math.Pow((xB-xA), 2) + 
+                   Math.Pow((yB-yA), 2) + 
+                   Math.Pow((zB-zA), 2));
+}
+double segmentLength=Math.Round(Decision(xA, xB, yA, yB, zA, zB), 2);
+Console.WriteLine($"Segment length  {segmentLength}");
+*/
+
+/*//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
 Console.Write("Enter a number: ");
-int Number = Convert.ToInt16(Console.ReadLine());
-string anyNumberText = Convert.ToString(Number);
-if (anyNumberText.Length > 2)
+int square = Convert.ToInt32(Console.ReadLine());
+
+void Square(int[] square)
 {
-  Console.WriteLine("Third digit is - " + anyNumberText[2]);
-}
-else 
-{
-  Console.WriteLine("...there is no third digit in this number");
-}
-*/
-/*//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-Console.Write("Enter a number for the day of the week: ");
-int dayNum = Convert.ToInt16(Console.ReadLine());
-void CheckingTheDayOfTheWeek (int dayNum) 
-{
-  if (dayNum == 6 || dayNum == 7) 
+  int counter = 0;
+  int length = square.Length;
+  while (counter < length)
   {
-  Console.WriteLine("this day is a day off");
+    square[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
   }
-  else if (dayNum < 1 || dayNum > 7) 
-  {
-    Console.WriteLine("there is no such day of the week");
-  }
-  else Console.WriteLine("this day is not a day off");
 }
-CheckingTheDayOfTheWeek(dayNum);
+
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  while(index < count)
+  {
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[square+1];
+Square(arry);
+PrintArry(arry);
 */
