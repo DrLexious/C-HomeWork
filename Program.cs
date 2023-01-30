0f0aff07f50251b3bba70b2d01c273a3ecd03fb1
Alex <120469966+DrLexious@users.noreply.github.com>
2023-01-30 19:57:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,73 +1,68 @@
-/*//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-int[] StringToNum(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-        {
-            count++;
-        }
-    }
-    int[] numbers = new int [count];
-    int index = 0;
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
-}
+/*//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-void ShowArray(int[] array)
+double[,] CreateRandom2DArray()
 {
-    for (int i = 0; i<array.Length; i++)
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of colums: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    double[,] array = new double[rows,colums];
+    for(int i=0; i<rows; i++)
+        for(int j=0; j<colums; j++)
+            array[i,j] = Convert.ToDouble(new Random().Next(minValue,maxValue+1))/10;
+    return array;
+}
+void Show2DArray(double[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
     {
-        Console.Write(array[i] + ", ");
+        for(int j=0; j<array.GetLength(1); j++)
+            Console.Write(array[i,j]+"\t");
+        Console.WriteLine();
     }
 }
-
-Console.Write("Enter numbers separated by comma: ");
-int[] numbers = StringToNum(Console.ReadLine());
-ShowArray(numbers);
-int sum = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > 0)
-    {
-        sum++;
-    }
-}
-Console.WriteLine($"Numbers greater than 0: {sum}");
+double[,] newArray = CreateRandom2DArray();
+Show2DArray(newArray);
 */
 
-/*//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-Console.WriteLine("Enter b1: ");
-float b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter k1: ");
-float k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter b2: ");
-float b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter k2: ");
-float k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number of row: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number of column: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+CreateRandom2DArray(numbers);
 
-float x = (-b2+b1)/(-k1+k2);
-float y = k2*x+b2;
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("No such element");
+}
+else
+{
+    Console.WriteLine($"Element of {n} row & {m} column equal {numbers[n-1,m-1]}");
+}
 
-Console.WriteLine($"Two straight lines intersect in point X: {x}, Y: {y}");
+Show2DArray(numbers);
+
+void CreateRandom2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)       
+            for (int j = 0; j < array.GetLength(1); j++)
+                array [i,j] = new Random().Next(-100, 100)/10;          
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + ",");  
+        Console.WriteLine(); 
+    }
+}
 */
